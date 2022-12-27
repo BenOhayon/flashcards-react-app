3f0d1eb03a3ca812f85b117ac4923be1056462be
Ben Ohayon <ben1679@gmail.com>
2022-12-27 15:30:45 +0200

    centered the flashcards

--- src/components/Flashcard/Flashcard.jsx
+++ src/components/Flashcard/Flashcard.jsx
@@ -6,14 +6,18 @@ export default function Flashcard({ flashcard }) {
 
   const [flip, setFlip] = useState(false)
   const [height, setHeight] = useState('initial')
+  const [width, setWidth] = useState('initial')
 
   const frontEl = useRef()
   const backEl = useRef()
 
   function setMaxHeight() {
     const frontHeight = frontEl.current.getBoundingClientRect().height;
+    const frontWidth = frontEl.current.getBoundingClientRect().width;
     const backHeight = backEl.current.getBoundingClientRect().height;
+    const backWidth = backEl.current.getBoundingClientRect().width;
     setHeight(Math.max(frontHeight, backHeight, 100));
+    // setWidth(Math.max(frontWidth, backWidth, 130));
   }
 
   useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
